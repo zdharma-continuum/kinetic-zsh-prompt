9fd5db2e89ae31e3132582cd1c4d56e2e091b315
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2020-02-01 22:29:23 +0100

    Add Zsh plugin file, gitignore, README.md and LICENSE

---
+++ kinetic-zsh-prompt.plugin.zsh
@@ -0,0 +1,22 @@
+# -*- mode: sh; sh-indentation: 4; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
+
+# Copyright (c) 2020 Sebastian Gniazdowski
+
+# According to the Zsh Plugin Standard:
+# http://zdharma.org/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html
+
+0=${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}
+0=${${(M)0:#/*}:-$PWD/$0}
+
+# Then ${0:h} to get plugin's directory
+
+if [[ ${zsh_loaded_plugins[-1]} != */kinetic-zsh-prompt && -z ${fpath[(r)${0:h}]} ]]
+then
+    fpath+=( "${0:h}" )
+fi
+
+# Standard hash for plugins, to not pollute the namespace
+typeset -gA Plugins
+Plugins[KINETIC_DIR]=${0:h}
+
+# vim:ft=zsh:tw=80:sw=4:sts=4:et:foldmarker=[[[,]]]
